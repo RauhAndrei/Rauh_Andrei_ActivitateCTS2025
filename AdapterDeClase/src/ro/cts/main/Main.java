c7568d82c98e98bf2a06c4b423004e063152de64
Rauh Andrei <rauhandrei69@gmail.com>
2025-04-06 17:07:04 +0300

    seminar 6 - adaptor de clase

---
+++ AdapterDeClase/src/ro/cts/main/Main.java
@@ -0,0 +1,20 @@
+package ro.cts.main;
+
+import ro.cts.clase.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Farmacie farmacie = new Farmacie("Catena");
+        MedicamentFarmacie medicamentFarmacie1 = new MedicamentFarmacie("aspacardin", "01/01/2026",true,57.4f);
+        MedicamentFarmacie medicamentFarmacie2 = new MedicamentFarmacie("teraflu", "21/03/2026",true,32.5f);
+
+        farmacie.vindeMedicament(medicamentFarmacie1);
+        farmacie.vindeMedicament(medicamentFarmacie2);
+
+        MedicamentSpital medicamentSpital = new MedicamentSpital("paracetamol", 24.9f);
+        //farmacie.vindeMedicament(medicamentSpital);
+        AdaptorClaseMedicament adaptorClaseMedicament = new AdaptorClaseMedicament("paracetamol",24.9f);
+        farmacie.vindeMedicament(adaptorClaseMedicament);
+    }
+}
+
